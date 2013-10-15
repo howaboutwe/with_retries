0b972ce91854526259fc4776feb214d553286722
Bryan Woods <bryanwoods4e@gmail.com>
2013-10-15 10:35:13 -0400

    Adds support for a `logger` parameter
    Will send a warn message to the given logger for more insight into what
    is being retried and why.

--- lib/with_retries.rb
+++ lib/with_retries.rb
@@ -4,11 +4,21 @@ module Kernel
   def with_retries(errors, params = {}, &block)
     attempts = params[:attempts] or
       raise ArgumentError.new("Attempts parameter not provided")
+    logger = params[:logger]
 
     begin
       yield
     rescue *errors
       attempts -= 1
+
+      if logger
+        times = (attempts == 1 ? "time" : "times")
+
+        logger.warn(
+          "Attempt failed: #{errors}. Retrying #{attempts} more #{times}"
+         )
+      end
+
       attempts > 0 ? retry : raise
     end
   end
